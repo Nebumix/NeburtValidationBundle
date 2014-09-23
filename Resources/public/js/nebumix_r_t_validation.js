b7c6b48108ba6b5136b7a8fcb3cfe2b6c924f002
Mario Alicicco <Nebumix@users.noreply.github.com>
2014-09-23 16:35:55 +0100

    Update nebumix_r_t_validation.js

--- Resources/public/js/nebumix_r_t_validation.js
+++ Resources/public/js/nebumix_r_t_validation.js
@@ -24,3 +24,31 @@
 
 	      return res;			
 	}
+
+
+	function check_field_full_name(name_form, name_field){
+		var res = "";
+
+		$.ajax({  
+	        url: Routing.generate('nebumixrt_validation_check'),  
+	        type: "POST",  
+	        async: false,
+			data: { item : $('#'+name_field+'').val(), name : name_field, nameForm : name_form },
+	        dataType: "html",
+	        success: function(msg) { 
+	              //alert(msg);
+	              res = msg;
+	              
+	              if(msg != 1){
+	              	$('#'+name_field+'_error').html(msg);
+	              }else{
+	              	$('#'+name_field+'_error').html("");
+	              }
+	        },
+	        error: function(){
+	          alert("ERROR!");
+	        } 
+	      }); 	
+
+	      return res;			
+	}
