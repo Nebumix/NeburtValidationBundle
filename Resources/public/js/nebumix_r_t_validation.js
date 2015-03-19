feb2dd524257e4db32d82bdeff55f22f724ed77c
Mario Alicicco <marioalicicco@MacBook-Pro-di-Mario.local>
2015-03-19 20:44:39 +0000

    tolta estensione da controller e puliti gli spazi

--- Resources/public/js/nebumix_r_t_validation.js
+++ Resources/public/js/nebumix_r_t_validation.js
@@ -1,44 +1,17 @@
-	/*function check_field(name_form, name_field){
-		var res = "";
-
-		$.ajax({  
-	        url: Routing.generate('nebumixrt_validation_check'),  
-	        type: "POST",  
-	        async: false,
-	        data: { item : $('#form_'+name_field).val(), name : name_field, nameForm : name_form },
-	        dataType: "html",
-	        success: function(msg) { 
-	              //alert(msg);
-	              res = msg;
-	              
-	              if(msg != 1){
-	              	$('#'+name_field+'_error').html(msg);
-	              }else{
-	              	$('#'+name_field+'_error').html("");
-	              }
-	        },
-	        error: function(){
-	          alert("ERROR!");
-	        } 
-	      }); 	
-
-	      return res;			
-	}*/
-
 
 	function check_field(name_form, name_field){
 		var res = "";
 
-		$.ajax({  
-	        url: Routing.generate('nebumixrt_validation_check'),  
-	        type: "POST",  
+		$.ajax({
+	        url: Routing.generate('nebumixrt_validation_check'),
+	        type: "POST",
 	        async: false,
 		data: { item : $('#'+name_field+'').val(), name : name_field, nameForm : name_form },
 	        dataType: "html",
-	        success: function(msg) { 
+	        success: function(msg) {
 	              //alert(msg);
 	              res = msg;
-	              
+
 	              if(msg != 1){
 	              	$('#'+name_field+'_error').html(msg);
 	              }else{
@@ -47,23 +20,23 @@
 	        },
 	        error: function(){
 	          alert("ERROR!");
-	        } 
-	      }); 	
+	        }
+	      });
 
-	      return res;			
+	      return res;
 	}
 
 
 	function check_field_check(name_form, name_field){
 		var res = "";
 
-		$.ajax({  
-	        url: Routing.generate('nebumixrt_validation_check'),  
-	        type: "POST",  
+		$.ajax({
+	        url: Routing.generate('nebumixrt_validation_check'),
+	        type: "POST",
 	        async: false,
 		data: { item : $( 'input:radio[name*='+name_field+']:checked' ).val(), name : name_field, nameForm : name_form },
 	        dataType: "html",
-	        success: function(msg) { 
+	        success: function(msg) {
 	              //alert(msg);
 	              res = msg;
 
@@ -73,10 +46,10 @@
 	              	$('#'+name_field+'_error').html("");
 	              }
 	        },
-	        error: function(){
+            error: function(){
 	          alert("ERROR!");
-	        } 
-	      }); 	
+	        }
+	      });
 
-	      return res;			
+	      return res;
 	}
